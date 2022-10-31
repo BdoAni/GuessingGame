2075c138746af3c76237f75ad8b9015837dd5069
BdoAni <bdoyani@gmail.com>
2022-10-31 15:08:26 -0700

    Display player name validation

--- game.py
+++ game.py
@@ -4,15 +4,21 @@ import random
 
 # Put your code here
 def guessing_game():
-    user_name = input("Howdy, what's your name?")
+    user_name = input("Howdy, what's your name? ")
+    while len(user_name) < 2:
+        user_name=input(f"{user_name} is not a valid name. ")
+        
     random_number=random.randint(1,100)
     print(random_number)
     user_guess=None
     num_guesses = 0
     print(f"{user_name}, I'm thinking of a number between 1 and 100!")
     print("Try to guess my number.")
-    while random_number !=user_guess:
-        user_guess=int(input('Your guess?'))
+    while random_number != user_guess:
+        user_guess=int(input('Your guess? '))
+        while user_guess < 1 or user_guess > 100:
+            num_guesses += 1
+            user_guess = int(input("That is a bad guess! Guess a number between 1 and 100!"))
         if user_guess>random_number:
             print('Your guess is too high, try again')
             num_guesses += 1
