5e78ba39f1f6920e34089e74a06783e55143a22e
BdoAni <bdoyani@gmail.com>
2022-10-31 14:04:40 -0700

    added print commit

--- game.py
+++ game.py
@@ -4,20 +4,24 @@ import random
 
 # Put your code here
 def guessing_game():
-    user_name = input("Hello player! What's your name?")
+    user_name = input("Howdy, what's your name?")
     random_number=random.randint(1,100)
     print(random_number)
     user_guess=None
     num_guesses = 0
+    print(f"{user_name}, I'm thinking of a number between 1 and 100!")
+    print("Try to guess my number.")
     while random_number !=user_guess:
-        user_guess=int(input("What number is your guess???"))
+        user_guess=int(input('Your guess?'))
         if user_guess>random_number:
-            print('Too high')
+            print('Your guess is too high, try again')
             num_guesses += 1
         elif user_guess < random_number:
-            print("Too low")
+            print("Your guess is too low, try again.")
             num_guesses +=1
         else: 
+            num_guesses +=1
             print(f'congratulations {user_name} number guess: {num_guesses}')
+            
 
 guessing_game()
