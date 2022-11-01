18330e2ca0e0473ba657450224ba79f346b08be3
BdoAni <bdoyani@gmail.com>
2022-11-01 10:07:45 -0700

    More play

--- game.py
+++ game.py
@@ -1,13 +1,11 @@
 import random
 """A number-guessing game."""
 # print('hi')
-
 # Put your code here
 def guessing_game():
     user_name = input("Howdy, what's your name? ")
     while len(user_name) < 2:
         user_name=input(f"{user_name} is not a valid name. ")
-        
     random_number=random.randint(1,100)
     print(random_number)
     user_guess=None
@@ -18,7 +16,7 @@ def guessing_game():
         user_guess=int(input('Your guess? '))
         while user_guess < 1 or user_guess > 100:
             num_guesses += 1
-            user_guess = int(input("That is a bad guess! Guess a number between 1 and 100!"))
+            user_guess = int(input("That is a bad guess! Guess a number between 1 and 100!   "))
         if user_guess>random_number:
             print('Your guess is too high, try again')
             num_guesses += 1
@@ -27,7 +25,19 @@ def guessing_game():
             num_guesses +=1
         else: 
             num_guesses +=1
-            print(f'congratulations {user_name} number guess: {num_guesses}')
+            print(f'Well done, {user_name}! You found my number in {num_guesses} tries!')
+            return num_guesses
             
 
-guessing_game()
+continue_play = True
+best_score=999999
+while continue_play:
+    new_score = guessing_game()
+    if new_score < best_score :
+        best_score = new_score
+        print(f'Your best score so far is {best_score}' )
+    user_cont=(input(f'Do you want to play more? Y / N')).upper() 
+    if user_cont == 'N':
+            print(f'game over! Your best score is {best_score}!!!')
+            continue_play=False        
+
